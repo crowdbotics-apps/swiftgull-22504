7c7b068377b0c44dd0efa3d84c16aad73af6d4e0
Crowdbotics <team@crowdbotics.com>
2020-11-11 02:40:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/swiftgull_22504/MainActivity.java
@@ -0,0 +1,15 @@
+package com.swiftgull_22504;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "swiftgull_22504";
+    }
+}
